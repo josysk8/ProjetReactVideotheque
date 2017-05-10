957b865f8ae5506ea67155e70810053636381e78
joseph <joseph.devarenne@viacesi.fr>
2017-05-10 15:27:53 +0200

    search with criteria

--- src/page/SearchMovieCriteria.js
+++ src/page/SearchMovieCriteria.js
@@ -9,26 +9,83 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  ScrollView,
+  Image,
+  TextInput,
+  Alert,
+  Picker
 } from 'react-native';
 
-export default class SearchMovie extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Search Movie By Criteria
-        </Text>
-      </View>
-    );
-  }
+export default class SearchMovieCriteria extends Component {
+
+  constructor(){
+      super();
+      this.state = {
+        movies: [],
+        genres: []
+      };
+    }
+
+    async getGenre() {
+      let response = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=58f8fe741b03b0ae4c9a2ed080e94041&language=fr-FR');
+
+      let responseJson = await response.json();
+      this.setState({
+        genres: responseJson.genres
+      });
+      return responseJson;
+    }
+
+    async getMovieByGenre(genreselected) {
+      let response = await fetch(' https://api.themoviedb.org/3/discover/movie?api_key=58f8fe741b03b0ae4c9a2ed080e94041&language=fr-FR&sort_by=release_date.desc&with_genres='+genreselected);
+
+      let responseJson = await response.json();
+      this.setState({
+        movies: responseJson.results
+      });
+      return responseJson;
+    }
+
+    componentDidMount() {
+      this.getGenre();
+    }
+
+    onEndReached()
+    {
+
+    }
+
+    render() {
+      return (
+        
+          <ScrollView style={styles.container}>
+          <Picker selectedValue={this.state.selectedGenre} 
+           onValueChange={(genreselect) => this.getMovieByGenre(genreselect)}
+           > 
+          {this.state.genres.map((genre, key) => {
+             return (
+            <Picker.Item label={genre.name} value={genre.id} /> 
+            );
+            })}
+          </Picker>
+          {this.state.movies.map((movie, key) => {
+             return (
+               <View key={key}>
+                <Image style={ {width: 150, height: 100} } source={ {uri: basePath + movie.poster_path } } />
+                <Text >{movie.original_title}</Text>
+               </View>
+             );
+          })}
+        </ScrollView>
+      );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 50,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
   welcome: {
@@ -42,5 +99,7 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 });
+
+const basePath = "https://image.tmdb.org/t/p/w500"
 
 AppRegistry.registerComponent('ProjetReactVideotheque', () => ProjetReactVideotheque);
